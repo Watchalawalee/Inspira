a763f28270498f8822cd01545dd6af98022c02b4
Suchx <145431627+Suchx@users.noreply.github.com>
2025-05-10 16:05:07 +0700

    แก้emailแจ้งเตือน

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 // db.js
 const mongoose = require('mongoose');
-require('dotenv').config();
-
+require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 let isConnected = false;
 
 const connectDB = async () => {
   if (isConnected) return;
   try {
+    console.log("⛳ MONGODB_URI =", process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
     console.log('✅ Connected to MongoDB');
